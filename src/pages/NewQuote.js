a0dc763f18465fadeb3be5c7dab9f3d51dddda01
hagleyson <hagleyson@gmail.com>
2022-01-11 16:04:57 -0300

    Preventing Possibly Unwanted Route Transitions with the prompt component

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -4,7 +4,6 @@ import QuoteForm from "../components/quotes/QuoteForm";
 const NewQuote = () => {
   const history = useHistory();
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
     history.push("/quotes");
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
