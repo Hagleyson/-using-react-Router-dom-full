6473776aaab35acc21ea43a7708b492ca726b79b
hagleyson <hagleyson@gmail.com>
2022-01-11 14:37:45 -0300

    Practice Redirecting & Extracting Params

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,13 @@
-const QuoteDetail=()=>{
-    return <h2> Quotes detail</h2>
-}
-export default QuoteDetail
+import { useParams } from "react-router";
+
+const QuoteDetail = () => {
+  const params = useParams();
+  return (
+    <>
+      <h2> Quotes detail</h2>
+      <p>{params.quoteId}</p>
+    </>
+  );
+};
+
+export default QuoteDetail;
